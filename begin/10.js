5fca8037e98d18057c98aa959598caab5b92708c
Umarov Mehrboz <umarovmehrboz@email.com>
2022-02-03 17:36:23 +0500

    reshil begin10

---
+++ begin/10.js
@@ -0,0 +1,7 @@
+var a = 4
+var b = 2
+S = Math.pow(a, 2) + Math.pow(b, 2)
+D = Math.pow(a, 2) - Math.pow(b, 2)
+F = Math.pow(a, 2) * Math.pow(b, 2)
+H = Math.pow(a, 2) / Math.pow(b, 2)
+console.log(S, D, F, H)
