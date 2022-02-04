edd5e90eaf14219ed43de611f93c60f8d55fdc4c
Umarov Mehrboz <umarovmehrboz@email.com>
2022-02-04 13:06:20 +0500

    reshil begin13

---
+++ begin/12.js
@@ -0,0 +1,5 @@
+let a = 3
+let b = 4
+c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2))
+p = a + b + c
+console.log(c, p)
---
+++ begin/13.js
@@ -0,0 +1,7 @@
+var PI = 3.14
+var r1 = 3
+var r2 = 2
+s1 = PI * Math.pow(r1, 2)
+s2 = PI * Math.pow(r2, 2)
+s3 = s1 - s2
+console.log(s1, s2, s3)
