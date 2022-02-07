389d7dc77889010d8d96fa15af0c5eabb67e8db5
Umarov Mehrboz <umarovmehrboz@email.com>
2022-02-07 14:02:37 +0500

    reshil begin23

---
+++ begin/20.js
@@ -0,0 +1,6 @@
+let x1=10;
+let x2=20;
+let y1=30;
+let y2=40;
+ D=Math.sqrt(Math.pow(x2-x1),2)+(Math.pow(y2-y1),2);
+ console.log(D);
--- begin/22.js
+++ begin/22.js
@@ -1,5 +1,5 @@
- let a=10;
- let b=20;
- A=b;
- B=a;
- console.log(A,B);
+let a = 10;
+let b = 20;
+a = b;
+b = a;
+console.log(a, b); 
