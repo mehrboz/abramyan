feede1f1623b8a16bcb06ce38bb05fc5f547ccd0
Umarov Mehrboz <umarovmehrboz@email.com>
2022-02-07 16:09:20 +0500

    reshil begin33

--- begin/27.js
+++ begin/27.js
@@ -1,8 +1,9 @@
+const A=2;
 A1=Math.pow(A,2);
-A2=Math.pow(A,3);
-A3=Math.pow(A,5);
-A4=Math.pow(A,10);
-A5=Math.pow(A,15);
-console.log(A1,A2,A3,A4,A5)
-const d=A1*A2*A3*A4*A5;
-console.log(d)
+A2=Math.pow(A,4);
+A3=Math.pow(A,8);
+console.log(A1);
+ console.log(A2);
+ console.log(A3);
+ D=A1*A2*A3;
+ console.log(D);
---
+++ begin/33.js
@@ -0,0 +1,6 @@
+const  x_kg=10;
+const a_somoni=20;
+const y_kg=1;
+data=a_somoni/x_kg;
+Y=y_kg*data;
+console.log(Y)
