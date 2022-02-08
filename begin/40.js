21bef749868fbe7619bd6b31d27942cf5704e284
Umarov Mehrboz <umarovmehrboz@email.com>
2022-02-08 15:04:53 +0500

    reshil begin40

---
+++ begin/40.js
@@ -0,0 +1,13 @@
+const A1=30;
+const A2=20;
+const B1=15;
+const B2=30;
+const C1=4;
+const C2=8;
+const D=A1*B2-A2*B1;
+console.log(D);
+const x=(C1*B2-C2*B1)/D;
+const y=(A1*C2-A2*C1)/D;
+console.log(x);
+console.log(y);
+
