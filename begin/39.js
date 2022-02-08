dd27efa73858fefd4b1e4d363a4d15bea79caa27
Umarov Mehrboz <umarovmehrboz@email.com>
2022-02-08 14:50:01 +0500

    reshil begin39

---
+++ begin/39.js
@@ -0,0 +1,9 @@
+const a = 1;
+const b = 9;
+const c = 1;
+D = Math.pow(b, 2) - 4 * a * c;
+console.log(D);
+x1 = -b + Math.sqrt(D) / 2 * a;
+x2 = -b - Math.sqrt(D) / 2 * a;
+console.log(x1)
+console.log(x2)
