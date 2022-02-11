4909f2e27de5197cd9026cfb00514b4b3cf07785
Umarov Mehrboz <umarovmehrboz@email.com>
2022-02-11 14:12:02 +0500

    reshil integer30

---
+++ integer/30.js
@@ -0,0 +1,23 @@
+const R=  1120
+const D=R+100;
+console.log(D)
+const D1=D+100
+console.log(D1);
+const D2=D1+100;
+console.log(D2);
+const D3=D2+100;
+console.log(D3);
+const D4=D3+100;
+console.log(D4); 
+const D5=D4+100;
+console.log(D5);
+const D6=D5+100;
+console.log(D6);
+const D7=D6+100;
+console.log(D7);
+const D8=D7+100;
+console.log(D8);
+const D9=D8+100;
+console.log(D9);
+const D10=D9+100;
+console.log(D10);
