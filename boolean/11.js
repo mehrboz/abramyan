79c19a38fa76222d06080673ad3fcbf19de5585d
Umarov Mehrboz <umarovmehrboz@email.com>
2022-02-11 15:22:23 +0500

    reshil boolean11

---
+++ boolean/11.js
@@ -0,0 +1,10 @@
+const a = 5;
+const b = 4;
+const S = a % 2 !== 0;
+const C = b % 2 !== 0;
+const S1 = a % 2 == 0;
+const C1 = b % 2 == 0;
+console.log(S);
+console.log(C);
+console.log(S1);
+console.log(C1);
