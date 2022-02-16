c489f2406a19941e7ce0aae92b8e54f2f1ef85e6
Umarov Mehrboz <umarovmehrboz@email.com>
2022-02-16 10:46:26 +0500

    reshil boolean36

--- boolean/35.js
+++ boolean/35.js
@@ -2,5 +2,5 @@ const x=parseInt(prompt('x='));
 const y=parseInt(prompt('y='));
 const x1=parseInt(prompt('x1='));
 const x2=parseInt(prompt('x2='));
-const D=(((x+y1)%2))==(((x1+y1)%2));
+const D=(((x+y)%2))==(((x1+y1)%2));
 console.log(D);
---
+++ boolean/36.js
@@ -0,0 +1,6 @@
+const x1=parseInt(prompt('x1='));
+const y1=parseInt(prompt('y1='));
+const x2=parseInt(prompt('x2='));
+const y2=parseInt(prompt('y2='));
+const D= ((x1+ y1)+(x2+y2));
+console.log(D);
