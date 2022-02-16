e14370255f744415c597993deae381751558feac
Umarov Mehrboz <umarovmehrboz@email.com>
2022-02-16 11:12:54 +0500

    reshil boolean38

---
+++ boolean/39.js
@@ -0,0 +1,6 @@
+const x1 = parseInt(prompt('x1='));
+const y1 = parseInt(prompt('y1='));
+const x2 = parseInt(prompt('x2='));
+const y2 = parseInt(prompt('y2='));
+const D = (((x1 - x2) == (y1 - y2)) && ((x1 - x2) == (-1) * (y1 - y2)) && (x1 == x2) && (y1 == y2));
+console.log(D)
