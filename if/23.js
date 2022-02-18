637735db454d33abe601512bac30ad2ef35448f8
Umarov Mehrboz <umarovmehrboz@email.com>
2022-02-18 17:05:36 +0500

    reshil if26

---
+++ if/23.js
@@ -0,0 +1,17 @@
+const x = parseInt(prompt('x='));
+const y = parseInt(prompt('y='));
+const x1 = parseInt(prompt('x1='));
+const y1 = parseInt(prompt('y1='));
+const x2 = parseInt(prompt('x2='));
+const y2 = parseInt(prompt('y2='));
+if (y > 0 && x > 0) {
+    console.log('choryaki 1')
+}
+else if (y > 0 && x < 0) {
+    console.log('choryaki 2')
+}
+else if (y < 0 && x == 0) {
+    console.log('choryaki 3');
+}else{
+console.log('choryaki 4')
+}
---
+++ if/26.js
@@ -0,0 +1,10 @@
+const x=parseInt(prompt('x='))
+if ( x<=0){
+    console.log(-x)
+}
+else if (0<x<2){
+    console.log(Math.pow(x,2))
+}
+else if (x>2){
+console.log(4)
+}
