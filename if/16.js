960df74fb8cd6407bd0b0a601543d7785b48e290
Umarov Mehrboz <umarovmehrboz@email.com>
2022-02-18 12:26:31 +0500

    reshil if16

---
+++ if/16.js
@@ -0,0 +1,12 @@
+const a = parseInt(prompt('a='));
+const b = parseInt(prompt('b='));
+const c = parseInt(prompt('c='));
+if ((a < b && b < c)) {
+    k = 2;
+} else {
+    k = -1
+}
+a1=a *k;
+b1=b * k;
+c1=c * k;
+console.log(a1, b1, c1);
