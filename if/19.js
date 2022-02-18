99590eabb0bcd885ca2ad3441eba19a99a3bcdaa
Umarov Mehrboz <umarovmehrboz@email.com>
2022-02-18 13:16:23 +0500

    reshil if19

---
+++ if/19.js
@@ -0,0 +1,13 @@
+const a = parseInt(prompt('a='));
+const b = parseInt(prompt('b='));
+const c = parseInt(prompt('c='));
+const d = parseInt(prompt('d='));
+if (a == b); {
+    if (a == d);
+    console.log(c)
+    console.log(d);
+}
+if (a == c) {
+    console.log(b);
+    console.log(a);
+}
