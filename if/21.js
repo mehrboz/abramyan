ac76ea61ab2be376d816816fdd4df6c66d319a04
Umarov Mehrboz <umarovmehrboz@email.com>
2022-02-18 16:08:18 +0500

    reshil if21

--- if/21.js
+++ if/21.js
@@ -1,10 +1,14 @@
 const x = parseInt(prompt('x='));
 const y = parseInt(prompt('y='));
-if (x == 0 && y == 0) x == 0; {
-    console.log(x);
-} if (x == 0 && y !== 0) x1 == 1;
-{
-    console.log(x1);
-} if (x == 0 && y == 0) x2 == 2;{
-    console.log(y);
+if (x == 0 && y == 0){
+console.log(0)
+}
+else  if (y == 0){
+console.log(1)
+}
+else if (x == 0){
+console.log(2 )
+}
+else{
+console.log(3);
 }
