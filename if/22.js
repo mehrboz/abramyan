15e0cabbcb08da8c3db665ca97d79ef158ba7f6b
Umarov Mehrboz <umarovmehrboz@email.com>
2022-02-18 16:37:38 +0500

    reshil if22

---
+++ if/22.js
@@ -0,0 +1,13 @@
+const x = parseInt(prompt('x='));
+const y = parseInt(prompt('y='));
+if (y > 0 && x > 0) {
+    console.log('choryaki 1')
+}
+else if (y > 0 && x < 0) {
+    console.log('choryaki 2')
+}
+else if (y < 0 && x == 0) {
+    console.log('choryaki 3');
+}else{
+console.log('choryaki 4')
+}
