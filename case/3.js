7266a5465bd95f1cb238e4f69516dfe16c8867a4
Umarov Mehrboz <umarovmehrboz@email.com>
2022-02-21 16:43:25 +0500

    reshil case3

---
+++ case/3.js
@@ -0,0 +1,55 @@
+var fasli_sol = parseInt(prompt("Введите число из диапазона 1-12: "));
+switch (fasli_sol) {
+    case 1:
+       console.log('januar');
+       console.log('zimiston')
+        break;
+    case 2:
+        console.log ('febriary')
+        console.log('zimiston')
+        break;
+    case 3:
+      console.log( 'march')
+      console.log('bahor');
+        break;
+    case 4:
+        console.log('april');
+        console.log('bahor');
+        break;
+    case 5:
+        console.log('may');
+        console.log('bahor');
+        break;
+    case 6:
+        console.log(' jun');
+        console.log('tobiston');
+        break;
+    case 7:
+        console.log('jul');
+        console.log('tobiston');
+        break;
+    case 8:
+        console.log('august');
+        console.log('tobiston');
+        break;
+    case 9:
+        console.log('september');
+        console.log('tiramoh');
+        break;
+    case 10:
+     console.log('october');
+     console.log('tiramoh');
+        break;
+    case 11:
+        console.log('nowember');
+        console.log('tiramoh')
+        break;
+    case 12:
+        console.log('desember');
+        console.log('zimiston');
+        break;
+        default:
+            fasli_sol = 'in fasl nest'
+}
+    console.log(fasli_sol)
+   
