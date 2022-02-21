c8c2e7c587066ff43969f83d84795b7f6eb403b3
Umarov Mehrboz <umarovmehrboz@email.com>
2022-02-21 16:56:51 +0500

    reshil case4

--- case/4.js
+++ case/4.js
@@ -1,52 +1,65 @@
-var moh = parseInt(prompt("Введите число из диапазона 1-12: "));
-switch (moh) {
+var fasli_sol = parseInt(prompt("Введите число из диапазона 1-12: "));
+switch (fasli_sol) {
     case 1:
        console.log('januar');
        console.log('zimiston')
+       console.log('31 ruz')
         break;
     case 2:
         console.log ('febriary')
         console.log('zimiston')
+        console.log('29 ruz')
         break;
     case 3:
       console.log( 'march')
       console.log('bahor');
+      console.log('31 ruz')
         break;
     case 4:
         console.log('april');
         console.log('bahor');
+        console.log('30 ruz')
         break;
     case 5:
         console.log('may');
         console.log('bahor');
+        console.log('31 ruz')
         break;
     case 6:
         console.log(' jun');
         console.log('tobiston');
+        console.log('30 ruz')
         break;
     case 7:
         console.log('jul');
         console.log('tobiston');
+        console.log('31 ruz')
         break;
     case 8:
         console.log('august');
         console.log('tobiston');
+        console.log('31 ruz')
         break;
     case 9:
         console.log('september');
         console.log('tiramoh');
+        console.log('30 ruz')
         break;
     case 10:
      console.log('october');
      console.log('tiramoh');
+     console.log('31 ruz');
+
         break;
     case 11:
         console.log('nowember');
-        console.log('tiramoh')
+        console.log('tiramoh');
+        console.log('30 ruz')
         break;
     case 12:
         console.log('desember');
         console.log('zimiston');
+        console.log('31 ruz')
         break;
         default:
             fasli_sol = 'in fasl nest'
