ce7975372860508de1bb79f06e90c3e546bca1d6
Umarov Mehrboz <umarovmehrboz@email.com>
2022-02-21 14:48:45 +0500

    reshil case1

---
+++ case/1.js
@@ -0,0 +1,29 @@
+var i = parseInt(prompt("Введите число из диапазона 1-7: "));
+switch (i) {
+    case 1:
+        i = ' dushanbe';
+        break;
+    case 2:
+        i = 'seshanbe';
+        break;
+    case 3:
+        i = 'chorshanbe';
+        break;
+    case 4:
+        i = 'panjshanbe';
+        break;
+    case 5:
+        i = 'juma';
+        break;
+    case 6:
+        i = 'shanbe';
+        break;
+    case 7:
+        i = 'yakshanbe';
+        break;
+
+
+    default:
+        i = "in khel ruz mavjud nest";
+}
+console.log(i)
