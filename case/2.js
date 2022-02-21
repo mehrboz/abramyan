d91d6f929307c94c35310a8c49b825c7fa824690
Umarov Mehrboz <umarovmehrboz@email.com>
2022-02-21 14:55:09 +0500

    reshil case2

---
+++ case/2.js
@@ -0,0 +1,21 @@
+var k = parseInt(prompt("Введите число из диапазона 1-7: "));
+switch (k) {
+    case 1:
+        k = ' bad';
+        break;
+    case 2:
+        k = 'gayriqanoatbakhsh';
+        break;
+    case 3:
+        k = 'qanotbakhsh';
+        break;
+    case 4:
+        k = 'khub';
+        break;
+    case 5:
+        k = 'aulo';
+        break;
+    default:
+        k = "nodurust";
+}
+console.log(k)
