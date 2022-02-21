883a798d1ec9ec1daa4d900f8389b667c1dee181
Umarov Mehrboz <umarovmehrboz@email.com>
2022-02-21 14:12:31 +0500

    reshil if30

---
+++ if/30.js
@@ -0,0 +1,13 @@
+const n=parseInt(prompt('n='))
+ if ((n%2==0)){
+    console.log('adadi  juft ')
+}
+else if ((n%2==0>99)){
+    console.log('adadi  jufti seraqama ')
+}
+else  if ((n%2!==0){
+    console.log('adadi  toq ')
+}
+ else if ((n%2!==0)>99){
+    console.log('adadi  toqi seraqama ')
+}
