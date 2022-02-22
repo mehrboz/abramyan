ae4b6408a161fa7b6e0ba514d7cc61373b4f9c91
Umarov Mehrboz <umarovmehrboz@email.com>
2022-02-22 14:08:43 +0500

    reshil case 10

---
+++ case/10.js
@@ -0,0 +1,21 @@
+switch (i) {
+    case 1:
+        i = 'garb';
+		console.log("gardish ba tarafi rost");
+        break;
+    case 2:
+        i = 'janub';
+		console.log('davomdihii harakat')
+        break;
+    case 3:
+        i = 'sharq';
+		console.log('gardish ba tarafi chap')
+        break;
+    case 4:
+        i = 'shimol';
+		console.log('samti avvala')
+        break;
+    default:
+        i = "in samt mavjud nest";
+}
+console.log(i)
