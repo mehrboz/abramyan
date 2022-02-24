59929a3d169ae62d588f4fcd5663e2339bfe235a
Umarov Mehrboz <umarovmehrboz@email.com>
2022-02-24 14:24:23 +0500

    reshil case 15

---
+++ case/15.js
@@ -0,0 +1,51 @@
+let n = parseInt(prompt('n='));
+switch (n) {
+    case 6:
+        n='shash';
+        break;
+    case 7:
+        n='haft';
+        break;
+    case 8:
+        n='hasht';
+        break;
+    case 9:
+        n='nuh';
+    case 10:
+        n='dah';
+        break; 
+        case 11:
+            n='valet';
+            break;
+    case 12:
+        n='damma';
+        break;
+    case 13:
+        n='karol';
+        break;
+    case 14:
+        n='tuz';
+    default:
+        n = "kartai az in kalon nest"
+}
+console.log(n);
+let m = parseInt(prompt('m='));
+switch (m) {
+    case 1:
+        m='siyoh';
+        break;
+    case 2:
+        m='chillik';
+        break;
+    case 3:
+        m='khisht';
+        break;
+    case 4:
+        m='dil';
+        break;
+
+    default:
+        m = 'digar namudi karta nest'
+}
+console.log(m)
+ 
