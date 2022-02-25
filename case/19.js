f55039e339db3ff0035cb0c2f17a61d2ccab1e73
Umarov Mehrboz <umarovmehrboz@email.com>
2022-02-25 13:48:40 +0500

    reshil case19

---
+++ case/19.js
@@ -0,0 +1,63 @@
+let r = parseInt(prompt('r='));
+switch (r) {
+  case 1:
+    r = 'mush'
+    break;
+  case 2:
+    r = 'gov'
+    break;
+  case 3:
+    r = 'palang'
+    break;
+  case 4:
+    r = 'khargush'
+    break;
+  case 5:
+    r = mohi
+    break;
+  case 6:
+    r = 'mor';
+    break;
+  case 7:
+    r = 'asp'
+    break;
+  case 8:
+    r = 'gusfand'
+    break;
+  case 9:
+    r = 'maymun '
+    break;
+  case 10:
+    r = 'murg'
+    break;
+  case 11:
+    r = 'sag'
+    break;
+  case 12:
+    r = 'khuk'
+    break;
+  default:
+    r = 'digar namudi sol nest'
+}
+console.log(r);
+let s = parseInt(prompt('s='));
+switch (s) {
+  case 1:
+    s = 'sabz'
+    break;
+  case 2:
+    s = 'surkh'
+    break;
+  case 3:
+    s = 'zard'
+    break;
+  case 4:
+    s = 'safed'
+    break;
+  case 5:
+    s = 'siyoh'
+    break;
+  default:
+    s = 'digar rang nest'
+}
+console.log(s);
