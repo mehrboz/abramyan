075b4fb0aeb96c749125890bb256145035e33d1d
Umarov Mehrboz <umarovmehrboz@email.com>
2022-03-02 11:38:51 +0500

    reshil for24

---
+++ for/24.js
@@ -0,0 +1,11 @@
+let x = parseInt(prompt('a='))
+let N = parseFloat(prompt('N='));
+var p = x;
+var s = x;
+var k = 0;
+for (i = 1; i <= N; i++) {
+  k += 2
+  p *= (-1) * x * x / (k * (k + 1))
+  s += p
+  console.log(s)
+}
