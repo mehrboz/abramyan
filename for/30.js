35d686572f79aa641bdff5a674d030de6971ec53
Umarov Mehrboz <umarovmehrboz@email.com>
2022-03-03 14:47:12 +0500

    reshil for30

---
+++ for/30.js
@@ -0,0 +1,12 @@
+let n = parseInt(prompt('n='))
+let A = parseInt(prompt('a='));
+let B = parseInt(prompt('b='));
+let H = (B - A) / n;
+console.log(H);
+let x = A;
+let y;
+for (i = 0; i <= n; i++) {
+    y = 1 - Math.sin(x);
+    x += H;
+    console.log(x);
+}
