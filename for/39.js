f524af7e3c0d76b3068e08f46d3c9b6a10a80e51
Umarov Mehrboz <umarovmehrboz@email.com>
2022-03-09 10:52:21 +0500

    reshil for 39

---
+++ for/39.js
@@ -0,0 +1,8 @@
+let A = parseInt(prompt('a='));
+let B = parseInt(prompt('b='));
+console.log("A = ", A);
+console.log("B = ", B);
+for (i = A; i <= B; i++) {
+    for (j = 1; j <= i; j++)
+        console.log(i)
+}
