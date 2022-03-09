c01c0e93acd5f43a70ebffbe0b022fdefd6f0c13
Umarov Mehrboz <umarovmehrboz@email.com>
2022-03-09 11:12:18 +0500

    reshil for 40

---
+++ for/40.js
@@ -0,0 +1,10 @@
+let A = parseInt(prompt('a='));
+let B = parseInt(prompt('b='));
+console.log("A = ", A);
+console.log("B = ", B);
+let k = 0;
+for (i = A; i <= B; i++) {
+    for (j = 1; j <= k; j++)
+        console.log(i)
+    k += 1
+}
