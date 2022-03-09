d3167f110bf27383fed01adf72575c86738be281
Umarov Mehrboz <umarovmehrboz@email.com>
2022-03-09 11:14:14 +0500

    reshil for 40

--- for/40.js
+++ for/40.js
@@ -4,7 +4,7 @@ console.log("A = ", A);
 console.log("B = ", B);
 let k = 0;
 for (i = A; i <= B; i++) {
-    for (j = 1; j <= k; j++)
-        console.log(i)
+  for (j = 1; j <= k; j++)
+  console.log(i)
     k += 1
 }
