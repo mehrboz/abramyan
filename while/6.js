ca59cfde5c4b88ad1333dbb39f1a917a275585d0
Umarov Mehrboz <umarovmehrboz@email.com>
2022-03-11 15:42:29 +0500

    reshi while 6

--- while/6.js
+++ while/6.js
@@ -1,10 +1,10 @@
 let n = parseInt(prompt('n='));
 let r = 1;
 if (n % 2 == 0) {
-    console.log(2)
+    console.log('juft=',2)
 }
-else {
-    console.log(1)
+else  {
+    console.log('toq=', 1)
 }
 let f = 1;
 while (n >= r) {
