5f37d45db4cbc85a611f698162b190e396e6042c
Umarov Mehrboz <umarovmehrboz@email.com>
2022-03-16 10:15:16 +0500

    reshil while9

--- while/9.js
+++ while/9.js
@@ -1,7 +1,7 @@
 let N = parseInt(prompt('n='));
 console.log(N);
 let K = 1;
-while (K * K <= N) {
+while (K <= N) {
     K += 1;
     console.log("K = ", K)
     console.log("3^k = ", Math.min(3 ** K));
