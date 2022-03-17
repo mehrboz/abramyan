e5a5203f8c4870b39ba47bf3fcb6e17e5ee45d0b
Umarov Mehrboz <umarovmehrboz@email.com>
2022-03-17 16:00:10 +0500

    reshil while10

--- series/10.js
+++ series/10.js
@@ -1,8 +1,6 @@
 let n = parseInt(prompt('n='));
-for (i = 1; i <= n; i++) {
-    if (i > 0) {
-        console.log('true')
-    } else {
-        console.log('false')
-    }
+if (n > 0) {
+    console.log('true')
+} else {
+    console.log('false')
 }
