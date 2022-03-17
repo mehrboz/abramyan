86e26dbe1248957d8d0e35a7732f3c37090f37de
Umarov Mehrboz <umarovmehrboz@email.com>
2022-03-17 12:23:55 +0500

    reshil while 24

---
+++ while/24.js
@@ -0,0 +1,14 @@
+let n = parseInt(prompt('n='))
+let f1 = 1;
+let f2 = 1;
+let f = 0;
+while (f < n) {
+    f = f2 + f1
+    f2 = f1
+    f1 = f
+}
+if (f == n) {
+    console.log('true')
+} else {
+    console.log('false')
+}
