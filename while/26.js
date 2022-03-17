f8ce6fc65540009a9aef957c6833e99cd11ffd55
Umarov Mehrboz <umarovmehrboz@email.com>
2022-03-17 12:44:29 +0500

    reshil whie 26

--- while/26.js
+++ while/26.js
@@ -1,12 +1,13 @@
-let n = parseInt(prompt('n='))
-let f1 = 1;
-let f2 = 1;
-let f = 0;
-let k = 2;
-while (f < n) {
+let n=parseInt(prompt('n='))
+let f1=1;
+let f2=1;
+let f=0;
+let k=2;
+while(f<n){
     k++
-    f = f2 + f1
-    f2 = f1
-    f1 = f
+    f=f2+f1
+    f2=f1
+    f1=f
 }
-console.log(k)
+console.log('f_k-2=',f2)
+console.log('f_k+2=',f1+f2);
