426797b85d96e22c7f2290dd70f06921d35519d3
Umarov Mehrboz <umarovmehrboz@email.com>
2022-03-31 12:56:34 +0500

    reshil mtrix 7

---
+++ matrix/5.js
@@ -0,0 +1,3 @@
+var ar = ["php", "php", "php", "php", "css", "css", "script", "script", "html", "html", "java"];
+var str = ar.toString()
+console.log(str)
---
+++ matrix/7.js
@@ -0,0 +1,3 @@
+var arr = ["php", "php", "php", "php", "css", "css", "script", "script", "html", "html", "java"];
+str=arr.join(',')
+console.log(str)
